94834119c6bd6d7aa3e55a51dc4c1d3d01bf23d9
Wilson <1117116@est.intec.edu.do>
2025-10-27 23:40:49 -0400

    firstController

--- src/routes/diariesRoute.ts
+++ src/routes/diariesRoute.ts
@@ -1,40 +1,11 @@
 import express from "express";
-import * as diaryServices from "../services/diaryService.ts";
-import toNewDiaryEntry from "../utils.ts";
+import DiariesController   from "../controllers/diaries.controller.ts";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send(diaryServices.getEntriesWithoutSensitiveInfo());
-});
-
-router.get("/:id", (req, res) => {
-    const diary = diaryServices.findById(+req.params.id)
-
-    return (diary != null)
-        ? res.send(diary) 
-        : res.sendStatus(404)
-});
-
-router.post("/", (req, res) => {
-    try {
-    const { date, weather, visibility, comment } = req.body;
-    const newDiaryEntry = toNewDiaryEntry(req.body);
-    res.json(newDiaryEntry);
-    } catch (e) {
-        res.status(400).send(e instanceof Error ? e.message : "Something went wrong");
-    }
-});
-
-router.delete("/:id", (req, res) => {
-    try{
-    const id = Number(req.params.id);
-    diaryServices.deleteDiary(id);
-    res.sendStatus(204);
-    } 
-    catch {
-        res.status(400).send("Id not found");
-    }    
-});
+router.get("/", DiariesController.getAllDiaries);
+router.get("/:id", DiariesController.getDiaryById);
+router.post("/", DiariesController.addNewDiary);
+router.post("/:id", DiariesController.deleteDiaryById);
 
 export default router;
