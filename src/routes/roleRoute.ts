8936e5f624b67713530d883bb2f76181986ba3e2
Wilson <1117116@est.intec.edu.do>
2025-10-30 17:36:32 -0400

    feat: remove DiariesController and associated routes

---
+++ src/routes/roleRoute.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import RoleController from "../controllers/Role.controller";
+
+const router: Router = Router();
+
+const { addNewRole, deleteRole, getAllRoles, updateRole, getRoleById } = RoleController;
+ 
+router.get("/roles", getAllRoles);
+router.post("/role", addNewRole);
+router.put("/role", updateRole);
+router.delete("/role", deleteRole);
+router.get("/role/:id", getRoleById);
+
+export default router;
